b5356f02df7053c9c7f67026a631a6abd3ac1f34
anOtherAnalyse <23277378+anOtherAnalyse@users.noreply.github.com>
2023-09-25 23:03:21 +0200

    UI: add plugin info

--- plugin/symless_plugin.py
+++ plugin/symless_plugin.py
@@ -1,14 +1,80 @@
+import base64
 import collections
 import importlib
 import inspect
+import os
 import pkgutil
 import sys
 from typing import Collection
 
 import idaapi
 
+import symless
 import symless.plugins as plugins
 import symless.utils.utils as utils
+
+
+class fixedBtn(idaapi.Form.ButtonInput):
+    def __init__(self, plugin: "SymlessPlugin"):
+        super().__init__(self.reload, "0")
+        self.plugin = plugin
+
+    def reload(self, code):
+        idaapi.show_wait_box("Reloading Symless..")
+
+        try:
+            # terminate all extensions
+            self.plugin.term()
+
+            # reload symless code
+            reload_plugin()
+
+            # rebind all extensions
+            self.plugin.find_extensions()
+
+        except Exception as e:
+            import traceback
+
+            utils.g_logger.critical(repr(e) + "\n" + traceback.format_exc())
+        finally:
+            idaapi.hide_wait_box()
+
+    def get_tag(self):
+        return "<Reload:%s%d:%s%s:%s:%s>" % (
+            self.tp,
+            self.id,
+            "+" if self.is_relative_offset else "",
+            self.width,
+            self.swidth,
+            ":" if self.hlp is None else self.hlp,
+        )
+
+
+class SymlessInfoForm(idaapi.Form):
+    def __init__(self, plugin: "SymlessPlugin"):
+        icon_path = os.path.join(os.path.abspath(symless.__path__[0]), "resources", "bigger_champi.png")
+        with open(icon_path, "rb") as file:
+            icon_b64 = base64.b64encode(file.read()).decode()
+
+        img_html = "<img src='data:image/png;base64,%s'>" % icon_b64
+        info_html = """
+            <div style='text-align: center; margin: 6px; font-size: 16px;'>
+                <pre style='font-size: 72px;'>Symless</pre>
+                <pre style='font-size: 16px; text-align: left;'>%s<br><br>Version: <b>%.1f</b></pre>
+            </div>
+        """ % (
+            symless.PLUGIN_DESC,
+            symless.PLUGIN_VERSION,
+        )
+
+        super().__init__(
+            "BUTTON YES NONE\nBUTTON CANCEL NONE\nSymless plugin\n{img}<|>{info}\n{reload}",
+            {
+                "img": idaapi.Form.StringLabel(img_html, tp=idaapi.Form.FT_HTML_LABEL, size=None),
+                "info": idaapi.Form.StringLabel(info_html, tp=idaapi.Form.FT_HTML_LABEL, size=None),
+                "reload": fixedBtn(plugin),
+            },
+        )
 
 
 # Symless plugin
@@ -57,28 +123,9 @@ class SymlessPlugin(idaapi.plugin_t):
 
     # debug - reload plugin action
     def run(self, args):
-        ok = idaapi.ask_yn(idaapi.ASKBTN_YES, "Do you want to reload Symless plugin ?")
-        if ok != idaapi.ASKBTN_YES:
-            return
-
-        idaapi.show_wait_box("Reloading Symless..")
-
-        try:
-            # terminate all extensions
-            self.term()
-
-            # reload symless code
-            reload_plugin()
-
-            # rebind all extensions
-            self.find_extensions()
-
-        except Exception as e:
-            import traceback
-
-            utils.g_logger.critical(repr(e) + "\n" + traceback.format_exc())
-        finally:
-            idaapi.hide_wait_box()
+        info = SymlessInfoForm(self)
+        info.Compile()
+        return info.Execute()
 
     # term all extensions
     def term(self):
